0933deca575c82d90987c80251ac61b545bb59ec
dserver <ppdavisito89@gmail.com>
2025-05-09 18:28:38 -0400

    Working with the News API

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+import json
+from http.client import responses
 
+import requests
+from datetime import date
+from flask import Flask, jsonify,render_template
+
+today = date.today()
 app = Flask(__name__)
+API_key = "c461613eec5144589c8d5a2ad1d64cd4"
 
-@app.route('/')
-def home():
+# Top Headlines in the US
+endpoint = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_key}"
+response = requests.get(endpoint)
+
+if response.status_code == 200:
+    data = response.json()
+else:
+    print("Server Down")
+
+
+
+@app.route('/news')
+def get_news():
     return render_template('index.html')
 
 
