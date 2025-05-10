4d29e4c948b8be2c9260da7fc8a95dc136ebedb8
dserver <ppdavisito89@gmail.com>
2025-05-10 15:53:37 -0400

    Working with the FrontEnd

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-import json
-from http.client import responses
-
 import requests
 from datetime import date
-from flask import Flask, jsonify,render_template
+from flask import Flask, render_template
 
 today = date.today()
 app = Flask(__name__)
 API_key = "c461613eec5144589c8d5a2ad1d64cd4"
 
 # Top Headlines in the US
-endpoint = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_key}"
-response = requests.get(endpoint)
+endpoint = f"https://newsapi.org/v2/top-headlines?"
+parameters = {"country": "us",
+              "apiKey": {API_key}
+              }
+response = requests.get(endpoint, params=parameters)
+data = response.json()
 
-if response.status_code == 200:
-    data = response.json()
-else:
-    print("Server Down")
+# articles = data['articles']
+# for i in range(10):
+#     if i < len(articles):
+#         article = articles[i]
+        # print(f"Processing article at index {i}: {article['title']}")
 
 
-
-@app.route('/news')
+@app.route('/', methods = ["GET", "POST"])
 def get_news():
-    return render_template('index.html')
-
+    articles = data['articles']
+    top_news = articles[0]
+    return render_template('index.html',
+                           article = articles,
+                           top_news=top_news)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
